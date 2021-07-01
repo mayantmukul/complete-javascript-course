80db5850b3ca3164017346115004d712a7d8defe
Mayant Mukul <mayant.mukul@dream11.com>
2021-07-01 19:05:29 +0530

    implement AJAX challenge 3

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,58 +3,29 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 const imagesContainer = document.querySelector('.images')
+
 ///////////////////////////////////////
 
-const wait = function(seconds) {
-    return new Promise(function(resolve, reject) {
-        setTimeout(() => {
-            console.log(`${seconds}s done!`)
-            resolve()
-        }, seconds * 1000)
-    })
-}
-
-const setImageSource = function(node, path) {
-    return new Promise(function(resolve, reject) {
+const createImage = function(path) {
+    return new Promise(function(resolve) {
+        const node = document.createElement('img')
         node.src = path
-        node.addEventListener('load', function() {
-            resolve(node)
-        })
-        node.addEventListener('error', function() {
+        node.addEventListener('load', () => {
             resolve(node)
         })
     })
 }
 
-const createImage = (imgPath) => {
-    return new Promise(function(resolve, reject) {
-        const imgNode = document.createElement('img')
-        setImageSource(imgNode, imgPath)
-            .then(node => {
-                imagesContainer.insertAdjacentElement('beforeend', node)
-                return resolve(node)
-            })
-            .catch(error => reject(error))
+const loadAll = async function(imgPaths) {
+    const imgNodePromises = imgPaths.map(path => createImage(path))
+    const imgs = await Promise.all(imgNodePromises)
+    imgs.forEach(img => {
+        img.classList.add('parallel')
+        imagesContainer.insertAdjacentElement('beforeend', img)
     })
 }
 
-let current;
-createImage('./img/img-1.jpg')
-    .then(res => {
-        console.log(res)
-        current = res
-        return wait(2)
-    })
-    .then(() => {
-        current.style.display = 'none'
-        return createImage('./img/img-2.jpg')
-    })
-    .then(res => {
-        console.log(res)
-        current = res
-        return wait(2)
-    })
-    .then(() => {
-        current.style.display = 'none'
-    })
-    .catch(err => console.error(err))
+;(async function() {
+    const paths = ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']
+    await loadAll(paths)
+})()
